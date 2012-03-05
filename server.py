25153d06c884659d71e6117a2613bd274cb147fc
wschurman <wschurman@gmail.com>
2012-03-04 23:13:59 -0500

    Server implementation

--- server.py
+++ server.py
@@ -3,8 +3,16 @@ from parse import parse
 
 import json
 import time
+import pymongo
+import psutil, os
 from threading import Thread, Lock
-from bottle import route, run, request, abort, get, post, delete, error
+from bottle import route, run, request, abort, get, post, delete, error, response
+
+from pymongo import Connection
+
+connection = Connection('ec2-184-73-79-244.compute-1.amazonaws.com', 27017)
+db = connection.query_db
+collection = db.query_collection
 
 query_threads = dict()
 qid_counter = 0
@@ -13,6 +21,7 @@ qid_lock = Lock()
 class QueryThread(Thread):
 	
 	def __init__(self, q, d, qid):
+		self.query = q
 		self.start_url = "http://www.google.com/search?q=" + q
 		self.max_depth = d
 		self.qid = qid
@@ -35,12 +44,15 @@ class QueryThread(Thread):
 		
 		insert_into_db(self.parse_async_result.result)
 	
-	def insert_into_db(data):
-		self.dbkey = "Hello" #FAKE
-		self.qstatus = "Done" #after insert to DB
-		pass
+	def insert_into_db(self, data):
+		global collection
+		self.dbkey = collection.insert(data)
+		self.qstatus = "Done"
 	
-	def get_status():
+	def get_query(self):
+		return self.query
+	
+	def get_status(self):
 		return {"status":self.qstatus, "crawlstatus":self.crawlstatus, "parsestatus":self.parsestatus, "dbkey":self.dbkey}
 
 # return either waiting, running, or done
@@ -51,23 +63,33 @@ def get_query_status(qid):
 
 @get('/api/status')
 def apistatus():
-	pass
+	#global connection
+	status = True
+	#status &= connection.server_info()
+	return "ok"
 
 @get('/api/hello')
 def hello():
-	return "hello world"
+	return "Hello, World!"
 
 @get('/api/queries')
 def list_queries():
-	return "Method Not Implemented"
+	if len(query_threads) == 0:
+		abort(204, "No Queries.")
+	retdict = dict()
+	for k, v in query_threads.iteritems():
+		retdict[k] = v.get_query
+	response.set_header('Content-Type', 'application/json')
+	return json.dumps(retdict)
 
+# call with a query and depth json field in the post body, e.g. {"query":"hello", "depth":4}
 @post('/api/queries')
 def run_query():
 	global qid_lock, qid_counter	
 	r = request.json
 	print r
 	if not r or not r["query"] or not r["depth"]:
-		abort(404, "Invalid call.")
+		abort(400, "Invalid call, bad request.")
 	else:
 		return "started monitor thread"
 		with qid_lock:
@@ -76,6 +98,7 @@ def run_query():
 		t = QueryThread(r.query, r.depth, qid)
 		query_threads[qid] = t
 		t.start()
+		response.status_code = 201
 		response.set_header('Content-Type', 'application/json')
 		return json.dumps({"query_id":qid})
 
@@ -85,7 +108,7 @@ def get_query(qid):
 		abort(404, "Query not found.")
 	else:
 		response.set_header('Content-Type', 'application/json')
-		return get_query_status(qid) # TODO: write get_query_status
+		return get_query_status(qid)
 
 
 @delete('/api/queries/<qid>')
@@ -93,13 +116,25 @@ def delete_query(qid):
 	if not qid in query_threads:
 		abort(404, "Query not found.")
 	else:
+		abort(501, "Not Implemented")
 		return "Method Not Implemented"
 
 @get('/api/server_stats')
 def get_server_stats():
-	return "Method Not Implemented"
+	p = psutil.Process(os.getpid())
+	response.set_header('Content-Type', 'application/json')
+	return json.dumps({
+		"num_threads":p.get_num_threads(),
+		"cpu_percent":p.get_cpu_percent(interval=0),
+		"memory":p.get_memory_info(),
+		"connections":p.get_connections(kind='all')
+	})
 
 # The following method is a catchall method
+
+@error(501)
+def error501(error):
+	return "API Method Not Implemented"
 
 @error(404)
 def error404(error):
